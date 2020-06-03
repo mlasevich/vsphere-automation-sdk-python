6ba1465b63f2a4c0699ad4a0da8bfdecb6c9107a
Michael "M3" Lasevich <Michael.Lasevich@bhnetwork.com>
2020-06-03 11:51:40 -0700

    Merge all dependencies into one package for now

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 import os
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='vsphere-automation-sdk-unofficial',
       version='1.31.0',
@@ -10,19 +10,15 @@ setup(name='vsphere-automation-sdk-unofficial',
       url='https://github.com/vmware/vsphere-automation-sdk-python',
       author='VMware, Inc.',
       license='MIT',
+      package_dir={'': 'src'},
+      package_data={'': ['*.properties']},
+      packages=find_packages(where='./src'),
       install_requires=[
         'lxml >= 4.3.0',
         'suds ; python_version < "3"',
         'suds-jurko ; python_version >= "3.0"',
         'pyVmomi >= 6.7',
-        'vapi-runtime @ file://localhost/{}/lib/vapi-runtime/vapi_runtime-2.15.0-py2.py3-none-any.whl'.format(os.getcwd()),
-        'vapi-client-bindings @ file://localhost/{}/lib/vapi-client-bindings/vapi_client_bindings-3.3.0-py2.py3-none-any.whl'.format(os.getcwd()),
-        'vapi-common-client @ file://localhost/{}/lib/vapi-common-client/vapi_common_client-2.15.0-py2.py3-none-any.whl'.format(os.getcwd()),
-        'vmc-client-bindings @ file://localhost/{}/lib/vmc-client-bindings/vmc_client_bindings-1.26.0-py2.py3-none-any.whl'.format(os.getcwd()),
-        'nsx-python-sdk @ file://localhost/{}/lib/nsx-python-sdk/nsx_python_sdk-2.5.1.0.1.15419398-py2.py3-none-any.whl'.format(os.getcwd()),
-        'nsx-policy-python-sdk @ file://localhost/{}/lib/nsx-policy-python-sdk/nsx_policy_python_sdk-2.5.1.0.1.15419398-py2.py3-none-any.whl'.format(os.getcwd()),
-        'nsx-vmc-policy-python-sdk @ file://localhost/{}/lib/nsx-vmc-policy-python-sdk/nsx_vmc_policy_python_sdk-2.5.1.0.1.15419398-py2.py3-none-any.whl'.format(os.getcwd()),
-        'nsx-vmc-aws-integration-python-sdk @ file://localhost/{}/lib/nsx-vmc-aws-integration-python-sdk/nsx_vmc_aws_integration_python_sdk-2.5.1.0.1.15419398-py2.py3-none-any.whl'.format(os.getcwd()),
-        'vmc-draas-client-bindings @ file://localhost/{}/lib/vmc-draas-client-bindings/vmc_draas_client_bindings-1.9.0-py2.py3-none-any.whl'.format(os.getcwd()),
+        'urllib3 (>=1.25.1)',
+        'werkzeug (>=0.14.1)'
       ]
       )
